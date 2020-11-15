41a11132d56d6a402594a66742d9757fcb5aa4ea
Hang Tran <ngochang29397@gmail.com>
2020-11-15 19:53:13 +0700

    wrap up project

--- 1988238_TranThiNgocHang/Program.cs
+++ 1988238_TranThiNgocHang/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
+
 
 namespace _1988238_TranThiNgocHang
 {
@@ -20,7 +22,8 @@ namespace _1988238_TranThiNgocHang
             string fileContent;
             try
             {
-                fileContent = File.ReadAllText(filename);
+                // get file in current directory instead of bin/Debug/netcoreapp3.1
+                fileContent = File.ReadAllText(@"../../../" + filename);
                 string[] words = fileContent.Split(' ');
                 int length = words.Length;
                 int i = 0;
@@ -42,7 +45,7 @@ namespace _1988238_TranThiNgocHang
         }
         public static void WriteStringToFile(string file_name, string content)
         {
-            System.IO.File.WriteAllText(file_name, content);
+            System.IO.File.WriteAllText(@"../../../" + file_name, content);
         }
         public static string ConvertDecArrayToHexString(int[] int_array)
         {
@@ -110,6 +113,103 @@ namespace _1988238_TranThiNgocHang
         // 4. end binary search
 
 
+        // 5. queue
+        internal class Queue
+        {
+            internal int front;
+            internal int rear;
+            internal int size;
+
+            internal int capacity;
+
+            internal int[] array;
+
+            public Queue(int capacity)
+            {
+                this.capacity = capacity;
+                front = this.size = 0;
+                rear = capacity - 1;
+                array = new int[capacity];
+            }
+        }
+
+        internal class QueueHelper
+        {
+            // Queue is full when size becomes equal to 
+            // the capacity 
+            internal bool IsFull(Queue queue)
+            {
+                return (queue.size == queue.capacity);
+            }
+
+            // Queue is empty when size is 0
+            internal bool IsEmpty(Queue queue)
+            {
+                return (queue.size == 0);
+            }
+
+            // Method to add an item to the queue. We will add items from rear of Queue.
+            // It changes rear and size
+            internal void Push(Queue queue, int item)
+            {
+                if (IsFull(queue))
+                {
+                    return;
+                }
+                queue.rear = (queue.rear + 1) % queue.capacity;
+                queue.array[queue.rear] = item;
+                queue.size = queue.size + 1;
+            }
+
+            // Method to remove an item from queue. We will remove items from front of Queue
+            // It changes front and size
+            internal void Pop(Queue queue)
+            {
+                if (IsEmpty(queue))
+                {
+                    Console.WriteLine("The Queue is empty");
+                    return;
+                }
+
+                int item = queue.array[queue.front];
+                queue.front = (queue.front + 1) % queue.capacity;
+                queue.size = queue.size - 1;
+                Console.WriteLine("Item deleted is {0}", item);
+            }
+
+            // Method to get front element of queue
+            internal void GetFrontElement(Queue queue)
+            {
+                if (IsEmpty(queue))
+                {
+                    Console.WriteLine("The Queue is empty");
+                    return;
+                }
+                Console.WriteLine("Front : {0}", queue.array[queue.front]);
+            }
+
+            // Method to get rear element of queue
+            internal void GetRearElement(Queue queue)
+            {
+                if (IsEmpty(queue))
+                {
+                    Console.WriteLine("The Queue is empty");
+                    return;
+                }
+                Console.WriteLine("Rear : {0}", queue.array[queue.rear]);
+            }
+
+            internal void PrintQueue(Queue queue)
+            {
+                for (int i = queue.front; i <= queue.rear; i++)
+                {
+                    Console.Write(queue.array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // end queue implementation
 
         // main function
         public static void Main(string[] args)
@@ -125,10 +225,55 @@ namespace _1988238_TranThiNgocHang
             WriteStringToFile(sorted_numbers_file_name, sorted_hex_array_string);
 
             // 4
-            int number_to_find = 36;
+            Console.WriteLine("Enter the number you want to find: ");
+            int number_to_find = int.Parse(Console.ReadLine());
             int position = BinarySearch(sorted_int_array, number_to_find);
-            Console.WriteLine(string.Concat("position of number ", number_to_find, " in this array is: ", position));
+            
+            if (position == -1)
+            {
+                Console.WriteLine("Not found!");
+            } else
+                Console.WriteLine(string.Concat("position of ", number_to_find, " in this array is: ", position));
+            {
 
+            }
+
+            // 5
+            //create a queue with int array length
+            Queue even_queue = new Queue(res.Length);
+            Queue odd_queue = new Queue(res.Length);
+            QueueHelper my_queue_helper = new QueueHelper();
+            foreach (var element in res)
+            {
+                if (element % 2 == 0)
+                {
+                    my_queue_helper.Push(even_queue, element);
+                }
+                else
+                {
+                    my_queue_helper.Push(odd_queue, element);
+                }
+            }
+            Console.WriteLine("Odd queue:");
+            my_queue_helper.PrintQueue(odd_queue);
+            Console.WriteLine("Even queue:");
+            my_queue_helper.PrintQueue(even_queue);
+            string odd_number_file_content = "";
+            string even_number_file_content = "";
+            string odd_queue_file_name = "odd_queue.txt";
+            string even_queue_file_name = "even_queue.txt";
+            for (int i = 0; i < odd_queue.size - 1; i++)
+            {
+                odd_number_file_content  += " " +odd_queue.array[i];
+            }
+            for (int i = 0; i < even_queue.size - 1; i++)
+            {
+                even_number_file_content += " " + even_queue.array[i];
+            }
+            WriteStringToFile(odd_queue_file_name, odd_number_file_content);
+            Console.WriteLine("Write odd numbers queue into " + odd_queue_file_name + '!');
+            WriteStringToFile(even_queue_file_name, even_number_file_content);
+            Console.WriteLine("Write even numbers queue into " + even_queue_file_name + '!');
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+﻿1. Đồ án này làm gì?
+- Thực hiện 6 yêu cầu trong đồ án cuối kì DTTX HK1 2020-2021
+- Link đề bài: https://docs.google.com/document/d/1hZ_ONnM8j2n1nNU4JVp4EkbwuCPAmtZK7IfGY_3o7AU/edit
+
+2. Cách thực thi và kiểm thử bài tập này:
+- Mở đồ án bằng Visual Studio
+- Trong file Program.cs (file chính chứa code), Click chọn nút "Build" để build project và tương tác nhập liệu cũng
+như xem kết quả ở Terminal của Visual Code
+- Đồ án này sẽ xuất ra 3 file sorted_numbers, odd_queue.txt và even_queue.txt ở trong directory cùng cấp với Program.cs
+- File hex_numbers.txt chứa các số nguyên làm đầu vào cho bài này, người dùng có thể thay đổi bằng các số khác, lưu ý:
+các số phải được cách nhau bằng dấu cách
